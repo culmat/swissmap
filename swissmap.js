c3d4ce0348cac1290788693f3353792119ceaebf
Matthias Cullmann <matthias.cullmann@gmail.com>
2014-12-02 19:17:03 +0100

    removed logging

--- swissmap.js
+++ swissmap.js
@@ -4,19 +4,15 @@ if(query == 'undefined') {
 }
 
 function loadFeature(res){
-		console.log(res)
 		$.ajax({
 			url : "https://api3.geo.admin.ch/rest/services/api/MapServer/"+res.layerBodId+"/"+res.featureId,
 			jsonp : "callback",
 			dataType : "jsonp",
 			success : function(response) {
 				var geom = response.feature.geometry;
-				console.log(geom);
 				var wgs = CH2WGS(geom.y,geom.x,0)
-				console.log(wgs)
 				var coords = [wgs.B, wgs.L]
 				map.setView(coords, 15);
-				console.log(res.label)
 				L.marker(coords).addTo(map)
 				.bindPopup(res.label).openPopup();
 			}
