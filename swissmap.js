f456c783667f36acafed67e7318d7d02d4c3cd58
Matthias Cullmann <matthias.cullmann@gmail.com>
2017-05-04 17:16:04 +0200

    replaced geo admin with osm

--- swissmap.js
+++ swissmap.js
@@ -1,45 +1,30 @@
 
-function loadFeature(res){
+function loadFeature(query){
+	    console.log(query)
 		$.ajax({
-			url : "https://api3.geo.admin.ch/rest/services/api/MapServer/"+res.layerBodId+"/"+res.featureId,
-			jsonp : "callback",
+			url : "http://nominatim.openstreetmap.org/search/"+query+"?format=json",
+			jsonp : "json_callback",
 			dataType : "jsonp",
 			success : function(response) {
-				var geom = response.feature.geometry;
-				var wgs = CH2WGS(geom.y,geom.x,0)
-				var coords = [wgs.B, wgs.L]
+				console.log(response);
+				var geom = response[0];
+				console.log(geom)
+				var coords = [geom.lat, geom.lon]
 				map.setView(coords, 15);
 				L.marker(coords).addTo(map)
-				.bindPopup(res.label).openPopup();
+				.bindPopup(geom.display_name).openPopup();
 			}
 		});	
-}
-
-function findFeature(query, callback){
-	$.ajax({
-		url : "http://api3.geo.admin.ch/rest/services/api/SearchServer",
-		jsonp : "callback",
-		dataType : "jsonp",
-		data : {
-			searchText : query,
-			origins : "address",
-			type : "locations"
-		},
-		success : function(response) {
-			callback(response);
-		}
-	});
 }
 
 
 $(function() {
 	var query = decodeURIComponent(window.location.href.split('?')[1])
 	if(query == 'undefined') {
-		location.href= location.href + '?lerchenstrasse 79 4059 basel 2701 basel ch bs'	
+		location.href= location.href + '?lerchenstrasse 79 basel'	
 	}
-	findFeature(query, function(response){
-		loadFeature(response.results[0].attrs);
-	})
+	console.log("bonjour thomas")
+	loadFeature(query);
 	
 });
 
